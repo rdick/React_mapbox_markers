e8f2f7efe654f0ae389874013d70df3f41fe65b9
Ryan Dick <rdick@ualberta.ca>
2020-05-27 07:04:51 -0700

    completed mapbox project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, Popup } from "react-map-gl"
+import * as parkData from "./data/skateboard-parks.json"
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 45.4211,
+    longitude: -75.6903,
+    zoom: 10,
+    width: '100vw',
+    height: '100vh'
+  })
+
+  const [selectedPark, setSelectedPark] = useState(null)
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedPark(null)
+      }
+    }
+    window.addEventListener("keydown", listener)
+
+    return () => {
+      window.removeEventListener("keydown", listener)
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/rdick/ckani9rv60mc81ilubjrtzfni"
+        onViewportChange={(viewport) => setViewport(viewport)}
+      >
+        {parkData.features.map(park => (
+          <Marker
+            key={park.properties.PARK_ID}
+            latitude={park.geometry.coordinates[1]}
+            longitude={park.geometry.coordinates[0]}
+          >
+            <button class="marker-btn" onClick={(e) => {
+              e.preventDefault()
+              setSelectedPark(park)
+
+            }}>
+              <img src="https://images.vexels.com/media/users/3/141204/isolated/preview/d5235e37e6c743eea70a18144667378e-skateboard-silhouette-3-by-vexels.png" />
+            </button>
+          </Marker>
+        ))}
+
+        {selectedPark ? (
+          <Popup
+            latitude={selectedPark.geometry.coordinates[1]}
+            longitude={selectedPark.geometry.coordinates[0]}
+            onClose={() => { setSelectedPark(null) }}
+          >
+            <div>
+              <h2>{selectedPark.properties.NAME}</h2>
+              <p>{selectedPark.properties.DESCRIPTIO}</p>
+            </div>
+          </Popup>
+        ) : null}
+      </ReactMapGL>
     </div>
   );
 }
